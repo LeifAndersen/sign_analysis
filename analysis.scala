ae437c330f8910982109d43b678a4b049d2df2af
Leif Andersen <leif@leifandersen.net>
2012-12-15 20:07:39 -0700

    Fixed the bug in preprocessor.

--- analysis.scala
+++ analysis.scala
@@ -1,35 +1,19 @@
-
-
-abstract class SExp {
-  def isList: Boolean
-  def isAtom: Boolean
-}
-
-class ListSExp(token: String) extends Statement {
-  override def isList = true
-  override def isAtom = false
-}
-
-class ASExp(token: List[Statement]) extends Statement {
-  override def isList = false
-  override def isAtom = true
-}
-
 object Expresion {
 
   val StatementMap = scala.collection.mutable.HashMap.empty[String,List[Any]];
 
-  def preprocess(code: List[Any]) = this match {
-    case List(List("label", label: String), rest: List) => {
+  def preprocess(code: List[Any]): Any = code match {
+    case List(List("label", label: String), rest: List[Any]) => {
       StatementMap += (label -> rest)
       preprocess(rest)
     }
-    case List(a, rest: List) => preprocess(rest)
+    case List(_, rest: List[Any]) => preprocess(rest)
     case _ => null
   }
 
   def main(args: Array[String]) {
-    preprocess(List(List("label", "lab"), 3))
-    println(StatementMap.type)
+    var code = List()
+    preprocess(List(List("label", "lab"), List("3")))
+    println(StatementMap)
   }
 }
